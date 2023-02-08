52f82107c2bf72a3574e5ef4286e3310a96598d7
joshua <joshua@debian-BULLSEYE-live-builder-AMD64>
2023-02-07 21:38:47 -0500

    back again after work

--- docs/docs.md
+++
@@ -1,1 +0,0 @@
-<h1> Hello World Testing </h1>
--- generators/extend_color_palette.py
+++ generators/extend_color_palette.py
@@ -10,11 +10,13 @@ import seaborn as sea
 # Check out the documentation for parameters that are supported for Extending Natura Naturans
 # https://glasbey.readthedocs.io/en/latest/index.html
 
-print(glasbey.extend_palette("#453B32", palette_size=16, lightness_bounds=(20, 40), chroma_bounds=(10, 30)))
+print(glasbey.extend_palette("#55aabb", palette_size=24, lightness_bounds=(20, 40), chroma_bounds=(10, 30)))
 sea.set()
-sea.palplot(glasbey.extend_palette("#453B32", palette_size=16, lightness_bounds=(20, 40), chroma_bounds=(10, 30)))
+sea.palplot(glasbey.extend_palette("#55aabb", palette_size=24, lightness_bounds=(20, 40), chroma_bounds=(10, 30)))
 plt.show()
 
 # Important to remember for changing things:
 # CHANGE BOTH THE HEX CODES & THE PARAMETERS for EXTENDING Natura Naturans
 
+# The command to extend the Color Palette:
+# python3 generators/extend_color_palette.py
